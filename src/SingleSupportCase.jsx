f5bd288b585f81c05a8daf80b225aa0d6ad5415d
Christian Boesen <christian.sisseck@gmail.com>
2021-03-24 19:31:27 +0100

    Delete video works

--- src/SingleSupportCase.jsx
+++ src/SingleSupportCase.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import { format} from 'date-fns'
+
+import axios from "axios";
+import Popup from 'reactjs-popup';
 import BigPicture from './../node_modules/bigpicture/src/BigPicture.js'
 
 function SingleSupportCase(props){
+
+const [showDetails, setshowDetails] = useState(false);
 
     /*
       {videos.map((video, index) => 
@@ -17,24 +22,63 @@ function SingleSupportCase(props){
 
     */
 
+const details = () => {
+  setshowDetails(!showDetails)
+}
+
 const startVideo = (e)=>{
-    console.log("video startet")
+   
     BigPicture({
 		el: e.target,
 		vidSrc: e.target.getAttribute('vidSrc'),
 	})
+   console.log("video startet")
+}
+
+const supportCaseVideoRef = useRef();
+
+const deleteVideo = (id) =>{ 
+
+supportCaseVideoRef.current.style.display = "none" 
+
+
+  axios.delete("/api/video/delete/" +id, {
+  }).then((response)=>{
+  console.log( response.data)
+})
+
 }
 
 
+
+
     return (
-        <div className="project">
+        <div ref={supportCaseVideoRef} onClick={details} className="project">
         <div className="actions" >
           <h3 > Date {format(new Date(Number(props.video.timestamp)), 'MM/dd - yyyy - HH:mm') }</h3>
           <div className="material-icons">
-          <span className="material-icons-outlined" vidSrc={ "http://" +props.video.video_url} onClick={startVideo}>play_circle_filled</span>
-          <span className="material-icons">delete</span>
+          <span className="material-icons" vidSrc={"http://"+props.video.video_url} onClick={startVideo}>play_circle_filled</span>
+         
+          <Popup trigger={<span className="material-icons" >delete</span>} position="left" closeOnDocumentClick>
+          {close => (<div className="popUpAreYouSure">Do you want to delete <span onClick={close}><button 
+            onClick={()=>{
+    
+            deleteVideo(props.video.video_id)
+            
+          }
+          }> YES </button> </span>   <button onClick={close}> NO</button>  </div>)}
+        </Popup>
+
+        
+         </div>
+        
         </div>
-        </div>
+
+        {showDetails && <div className="details">
+        <label>Browser type: </label> <div>{props.video.browser_type}</div>
+        <label>Problem: </label> <div>{props.video.issue_description}</div>
+        <label>link: </label> <div>{props.video.website_url}</div>
+        </div>}
         
         </div>
       );
